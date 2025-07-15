fc6bc9afaa3a8ac422aec91f09c513df7abb4cfc
alebars2023 <ale.bars2023@gmail.com>
2025-07-15 22:44:11 +0300

    Delete jsmain.js

--- jsmain.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.program').forEach(program => {
-  program.addEventListener('click', function () {
-    document.querySelectorAll('.program').forEach(p => p.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
